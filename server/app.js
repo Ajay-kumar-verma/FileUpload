9c1bc02151ee1c80c3fdd8363def9ea161fcddc4
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-11-13 16:04:06 +0530

    google drive file upload is done

---
+++ server/app.js
@@ -0,0 +1,46 @@
+import  fs from  'fs';
+import  {google}   from  'googleapis'
+
+const google_Folder_Api= '1ynBlK82IFjk950U0KZTde9mmfPoX3Wxk';
+
+
+async function uploadFile(){
+
+    try {
+        const auth = new google.auth.GoogleAuth({ 
+        keyFile:'./image.json',
+        scopes :['https://www.googleapis.com/auth/drive'] 
+
+        }) 
+
+    const drive = google.drive({
+        version:'v3',
+        auth
+    })    
+
+  const fileName = {
+    'name':'image.jpeg',
+     'parents':[google_Folder_Api]
+  }   
+  
+  const media = {
+    MimeType : 'imgae/jpg',
+    body:fs.createReadStream('./image.jpeg')
+  }
+
+  const resp = await drive.files.create({
+    resource:fileName,
+    media,
+    field:'id'
+  })
+
+return resp.data;
+} catch (error) {
+     console.log({error})   
+    }
+} 
+
+
+uploadFile().then(res=>console.log({res}));
+
+
